5a04b89c7d6cf2b9a4bdeb5ada3395d6744ad886
Mykhailo Petrov <mykhailo.petrov@schindler.com>
2025-01-06 23:33:57 +0200

    Create header and footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from '@/components/shared/Footer'
+import Header from '@/components/shared/Header'
+
 export default function Layout({
 	children,
 }: Readonly<{
@@ -5,7 +8,9 @@ export default function Layout({
 }>) {
 	return (
 		<div className="flex h-screen flex-col">
+			<Header />
 			<main className="flex-1 wrapper">{children}</main>
+			<Footer />
 		</div>
 	)
 }
