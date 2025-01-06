e16a9206c92768309fa645cade65663154bd6392
Mykhailo Petrov <mykhailo.petrov@schindler.com>
2025-01-06 03:34:07 +0200

    Add shadcn, constants and images

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+
+export const metadata = {
+	title: 'Home',
+}
+
+const HomePage = () => {
+	return (
+		<div>
+			Home Page
+			<Button>Click me</Button>
+		</div>
+	)
+}
+
+export default HomePage
